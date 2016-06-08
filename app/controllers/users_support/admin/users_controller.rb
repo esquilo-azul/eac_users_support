18b0d2467673e2ca387a8a23b7347c6aa4ac784a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-06-08 16:07:36 -0300

    Usuário criado pelo CRUD administrativo tem senha igual ao email. Refs #311.

--- app/controllers/users_support/admin/users_controller.rb
+++ app/controllers/users_support/admin/users_controller.rb
@@ -2,6 +2,13 @@ module UsersSupport
   module Admin
     class UsersController < ::UsersSupport::ApplicationController
       active_scaffold UsersSupport::User do |conf|
+        conf.columns.exclude :encrypted_password
+      end
+
+      protected
+
+      def before_create_save(record)
+        record.password = record.email
       end
     end
   end
