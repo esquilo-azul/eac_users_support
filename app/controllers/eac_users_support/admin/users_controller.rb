655fcdc3344beefaf5fcbea822a755e3626c0196
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-03-15 16:23:45 -0300

    Remove authentication requeriment for EacUsersSupport::Admin::UsersController.

--- app/controllers/eac_users_support/admin/users_controller.rb
+++ app/controllers/eac_users_support/admin/users_controller.rb
@@ -1,8 +1,6 @@
 module EacUsersSupport
   module Admin
     class UsersController < ::EacUsersSupport::ApplicationController
-      before_action :authenticate_user!
-
       active_scaffold ::EacUsersSupport::User do |conf|
         conf.columns.exclude :encrypted_password
       end
