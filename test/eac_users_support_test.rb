9cd1b49d8300bf78399a0d6c4dcf8a4d5a948a67
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-09-23 14:25:53 -0300

    Remove minitest.

--- test/eac_users_support_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class EacUsersSupportTest < ActiveSupport::TestCase
-  test 'truth' do
-    assert_kind_of Module, EacUsersSupport
-  end
-end
