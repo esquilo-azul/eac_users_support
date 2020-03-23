147fb8870cebdee6a85c28af8958158dc57370ee
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-03-23 18:06:12 -0300

    EacUsersSupport::Tasks::PasswordReset: refact.

--- lib/eac_users_support/tasks/password_reset.rb
+++ lib/eac_users_support/tasks/password_reset.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/simple_cache'
+
 module EacUsersSupport
   module Tasks
     class PasswordReset
+      include ::EacRubyUtils::SimpleCache
+
       def initialize(email, administrator)
         @email = email
         @administrator = administrator
       end
 
       def run
-        u = User.find_by(email: @email)
-        if u
+        if found_user
           Rails.logger.info "Usuário encontrado com o email \"#{@email}\""
-          u.update!(attributes_to_update)
+          found_user.update!(attributes_to_update)
         else
           Rails.logger.info "Usuário não encontrado com o email \"#{@email}\". Criando..."
           User.create!({ email: @email }.merge(attributes_to_update))
@@ -26,6 +29,10 @@ module EacUsersSupport
         r = { password: @email, password_confirmation: @email }
         r[:administrator] = true if @administrator
         r
+      end
+
+      def found_user_uncached
+        ::EacUsersSupport::User.find_by(email: @email)
       end
     end
   end
