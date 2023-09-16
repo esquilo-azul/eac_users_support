9b22a351aa7b161007d548596d85fb406c53db5d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-09-16 19:30:23 -0300

    config/initializers/menus.rb.

---
+++ config/initializers/menus.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'eac_rails_utils/patches/application'
+
+::Rails.application.root_menu.group(:admin).group(:eac_users_support, :eac_users_support)
+       .within do |g|
+  g.action(:admin_users).label(-> { ::EacUsersSupport::User.plural_name })
+end
